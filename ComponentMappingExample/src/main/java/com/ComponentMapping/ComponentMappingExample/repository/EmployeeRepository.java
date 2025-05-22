646a63910a1c84b4b2ba3bdecba5ceecc02f031f
RahulTN2002 <Rahul.TN@cognizant.com>
2025-05-22 14:27:56 +0530

    Delete ComponentMappingExample directory

--- ComponentMappingExample/src/main/java/com/ComponentMapping/ComponentMappingExample/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ComponentMapping.ComponentMappingExample.repository;
-
-import com.ComponentMapping.ComponentMappingExample.entities.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
-}
